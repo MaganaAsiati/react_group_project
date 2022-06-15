38d9c32861e3883f30190d82a117ee067a88e20e
jaujau088 <jaujau088@gmail.com>
2022-06-15 12:50:45 +0200

    Move useEffect for fetching data to App.js

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,15 +1,8 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Table from './missions_components/Table';
-// import { fetchMissions } from '../../Redux/missions/missions';
 import './Missions.css';
 
 const Missions = () => {
-  const dispatch = useDispatch();
-  // Call dispatch fetch only once on mount
-  useEffect(() => dispatch(
-    // fetchMissions()
-  ), []);
   const data = useSelector((state) => state.missionsReducer);
 
   return (
