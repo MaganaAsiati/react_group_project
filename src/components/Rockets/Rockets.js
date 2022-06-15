4cb811f07a276571aba0ee593b856bbe3de7f797
MaganaAsiati <assia.magana@gmail.com>
2022-06-15 11:47:20 +0200

    use use sector and dispatch to dispatch  our data

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getRockets } from '../../Redux/Rockets/rocket';
 
-export default function Rockets() {
-  return (
-    <div>Rockets</div>
-  );
+function Rockets() {
+  const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (rockets.length === 0) {
+      dispatch(getRockets());
+    }
+  }, []);
+  return <div>Rockets</div>;
 }
+
+export default Rockets;
