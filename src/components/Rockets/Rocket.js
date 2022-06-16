5606f34c2e8096fce71a3888fd9892ff86d40d5a
MaganaAsiati <assia.magana@gmail.com>
2022-06-17 00:39:52 +0200

    fix linters

--- src/components/Rockets/Rocket.js
+++ src/components/Rockets/Rocket.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Rocket.css';
 import { useDispatch } from 'react-redux';
 import { bookingAction, cancelBookingAction } from '../../Redux/Rockets/rocket';
 
@@ -17,35 +18,35 @@ export default function Rocket(props) {
     <div className="rocket-container">
       <div className="rocket-image">
         <img className="rocket-img" src={rocket.flickr_images} alt="Rocket" />
-        <div>
-          <h1 className="rocket-name">{rocket.name}</h1>
-          <p className="rocket-description">
-            {rocket.reserved && (
-              <span className="bg-cyan-600 mr-3 text-white rounded">
-                Reserved
-              </span>
-            )}
-            {rocket.description}
-          </p>
-
-          {rocket.reserved ? (
-            <button
-              type="button"
-              onClick={cancelBooking}
-              className="btn cancelBookingAction"
-            >
-              Cancel Reservation
-            </button>
-          ) : (
-            <button
-              type="button"
-              onClick={bookRocket}
-              className="btn bookingAction"
-            >
-              Reserve Rocket
-            </button>
+      </div>
+      <div className="rocket-right">
+        <h1 className="rocket-name">{rocket.name}</h1>
+        <p className="rocket-description">
+          {rocket.reserved && (
+            <span className="reverted">
+              Reserved
+            </span>
           )}
-        </div>
+          {rocket.description}
+        </p>
+
+        {rocket.reserved ? (
+          <button
+            type="button"
+            onClick={cancelBooking}
+            className="cancelBookingAction"
+          >
+            Cancel Reservation
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={bookRocket}
+            className="bookingAction"
+          >
+            Reserve Rocket
+          </button>
+        )}
       </div>
     </div>
   );
